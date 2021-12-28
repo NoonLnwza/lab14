274d4e923083141aab4c059fe5d960d28c12e7df
Pubet Choompusueb <pubet50@gmail.com>
2021-12-28 11:40:30 +0700

    completed

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -17,3 +17,21 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double A[],int N,double B[]){
+    double sum=0,sigma=0,GM=1,HM=0,max=A[0],min=A[0];
+    for(int i=0;i<N;i++){
+        sum+=A[i];
+        sigma+=pow(A[i],2);
+        GM=GM*A[i];
+        HM+=(1/A[i]);
+        if(A[i]>max) max=A[i];
+        if(A[i]<min) min=A[i];
+    }
+    B[0]=sum/N;
+    B[1]=sqrt((sigma/N)-pow(B[0],2));
+    B[2]=pow(GM,pow(N,-1));
+    B[3]=N/HM;
+    B[4]=max;
+    B[5]=min;
+}
